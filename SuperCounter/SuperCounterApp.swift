00f2248e45d5e141ba62a5fb5c59c6dbadf4d7c7
Nonprawich I <40120130+npwitk@users.noreply.github.com>
2024-12-24 19:18:50 +0700

    Initial Commit

---
+++ SuperCounter/SuperCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperCounterApp.swift
+//  SuperCounter
+//
+//  Created by Nonprawich I. on 24/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SuperCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
